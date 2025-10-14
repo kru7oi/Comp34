ade4e23d4a1b093897850aa3b38a8006b0953b3d
vboxuser <vboxuser@WIN10>
2025-10-14 10:07:13 +0300

    Добавьте файлы проекта.

---
+++ Элементы_компоновки/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Элементы_компоновки.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string Fullname { get; set; } = null!;
+
+    public DateOnly DateOfBirth { get; set; }
+
+    public int PositionId { get; set; }
+
+    public string Phone { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public int DepartmentId { get; set; }
+
+    public DateOnly DateOfEmployment { get; set; }
+
+    public bool IsVacation { get; set; }
+
+    public string Gender { get; set; } = null!;
+
+    public virtual Department Department { get; set; } = null!;
+
+    public virtual Position Position { get; set; } = null!;
+}
---
+++ Элементы_компоновки/Models/EmployeeAppDbContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Элементы_компоновки.Models;
+
+public partial class EmployeeAppDbContext : DbContext
+{
+    public EmployeeAppDbContext()
+    {
+    }
+
+    public EmployeeAppDbContext(DbContextOptions<EmployeeAppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Position> Positions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=EmployeeAppDb;TrustServerCertificate=true;Trusted_Connection=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.ToTable("Department");
+
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.ToTable("Employee");
+
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Fullname).HasMaxLength(100);
+            entity.Property(e => e.Gender)
+                .HasMaxLength(1)
+                .IsUnicode(false)
+                .IsFixedLength();
+            entity.Property(e => e.Phone).HasMaxLength(11);
+
+            entity.HasOne(d => d.Department).WithMany(p => p.Employees)
+                .HasForeignKey(d => d.DepartmentId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Employee_Department");
+
+            entity.HasOne(d => d.Position).WithMany(p => p.Employees)
+                .HasForeignKey(d => d.PositionId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Employee_Position");
+        });
+
+        modelBuilder.Entity<Position>(entity =>
+        {
+            entity.ToTable("Position");
+
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
