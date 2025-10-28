683ac5f5315008ea0d5a3f6be02eacef60dc73d8
vboxuser <vboxuser@WIN10>
2025-10-28 09:00:47 +0300

    Сделали фильтрацию

--- Элементы_компоновки/View/Pages/EmployeesPage.xaml.cs
+++ Элементы_компоновки/View/Pages/EmployeesPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Элементы_компоновки.Models;
 
 namespace Элементы_компоновки.View.Pages
 {
@@ -20,9 +21,16 @@ namespace Элементы_компоновки.View.Pages
     /// </summary>
     public partial class EmployeesPage : Page
     {
+        // Промежуточный список отделов
+        List<Department> departments = App.context.Departments.ToList();
         public EmployeesPage()
         {
+            // Отрисовывает элементы интерфейса и придаёт им функциональность
             InitializeComponent();
+
+            // Добавляем "отдел" в промежуточный список
+            departments.Insert(0, new() { Name = "Все отделы"});
+            FilterCmb.ItemsSource = departments;
 
             EmployeesLv.ItemsSource = App.context.Employees.ToList();
             // ItemsSource - свойство, которое представляет собой источник данных для элемента
@@ -52,6 +60,24 @@ namespace Элементы_компоновки.View.Pages
             else
             {
                 EmployeesLv.ItemsSource = App.context.Employees.Where(employee => employee.Fullname.Contains(SearchTb.Text)).ToList();
+            }
+        }
+
+        private void FilterCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // 1. Получить выбранный отдел из выпадающего списка
+            // 2. Получить полный список сотрудников
+            // 3. Произвести в нём фильтрацию по названию отдела
+            // 4. Полученный список передать в качестве источника элементов в ListView
+            Department? selectedDepartment = FilterCmb.SelectedItem as Department;
+
+            if (selectedDepartment.Name == "Все отделы")
+            {
+                EmployeesLv.ItemsSource = App.context.Employees.ToList();
+            }
+            else
+            {
+                EmployeesLv.ItemsSource = App.context.Employees.Where(employee => employee.DepartmentId == selectedDepartment.Id).ToList();
             }
         }
     }
